77b48b94c124c84fe04b8b41a63809bfb7bf9b2f
Ryan Ponthier <ryanjponthier@gmail.com>
2018-04-25 03:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function dec(a) {
 function makeInt(a) {
   return parseInt(a, 10)
 }
-function describeDecimal(b) {
-  return parseFloat('2.222')
+function describeDecimal(a) {
+  return parseFloat(2.222)
 }
